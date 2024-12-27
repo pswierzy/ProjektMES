e119d7fa7b4e4dc88d2d81b7659989d7a7ef659a
pswierzy <pswierzy@student.agh.edu.pl>
2024-12-27 03:09:02 +0100

    Changed e(x) to return lambda and added E(x)

--- projektRurki.py
+++ projektRurki.py
@@ -1,6 +1,6 @@
 import math
 
-def gauss_legendre_integral_manual(f, a, b):
+def gauss_legendre_integral(f, a, b):
     """
     Oblicza całkę funkcji f na przedziale [a, b] za pomocą metody Gaussa-Legendre.
     Węzły i wagi są ustalane ręcznie dla n = 2 lub n = 3.
@@ -9,7 +9,6 @@ def gauss_legendre_integral_manual(f, a, b):
         f (function): Funkcja do scałkowania.
         a (float): Początek przedziału całkowania.
         b (float): Koniec przedziału całkowania.
-        n (int): Liczba węzłów Gaussa (obsługiwane: 2, 3).
     
     Zwraca:
         float: Przybliżona wartość całki.
@@ -32,34 +31,51 @@ def gauss_legendre_integral_manual(f, a, b):
     return integral * det_jacobian
 
 
-def e_i(i, N, x):
+def e(i, N):
     '''
-    Oblicza wartość funkcji ei(x) w zależności od danego N na przydziale [0,2].
-    Funkcja ei to funkcja która jest równa 0 dla x-ów oprócz xi, dla którego jest równa 1.
+    Tworzy funkcję ei(x) w zależności od danego N na przedziale [0, 2].
+    Funkcja ei to funkcja, która jest równa 0 dla x-ów oprócz xi, dla którego jest równa 1.
 
     Parametry:
         i (int): Numer x-a.
-        N (int): Łączna ilość podziałów.
-        x (float): Wartość dla której jest obliczona ta funkcja.
+        N (int): Łączna liczba podziałów.
 
     Zwraca:
-        float: wynik ei(x).
+        function: Funkcja ei(x) jako lambda.
     '''
-    
-    # Przypadek dla i=0
-    if i==0:
-        next_x = 2 / N
+    x_i = 2 * i / N
+    next_x = x_i + 2 / N
+    previous_x = x_i - 2 / N
 
-        if x >= next_x: return 0
-        return 1-x/next_x
-    
-    # Przypadek dla i=/=0
-    else:
-        x_i = 2*i/N
-        next_x = x_i + 2/N
-        previous_x = x_i - 2/N
+    return lambda x: (
+        0 if x >= next_x or x <= previous_x 
+        else (x - previous_x) / (x_i - previous_x) if x > previous_x and x <= x_i 
+        else 1 - (x - x_i) / (next_x - x_i)
+    )
 
-        if x>=next_x or x<=previous_x: return 0
-        if x>previous_x and x<=x_i:
-            return (x-previous_x) / (x_i - previous_x)
-        return 1 - (x - x_i) / (next_x - x_i)
+def e_prim(i, N):
+    '''
+    Tworzy funkcję e'(x) w zależności od danego N na przedziale [0, 2].
+    Funkcja e' to pochodna funkcji e.
+
+    Parametry:
+        i (int): Numer x-a.
+        N (int): Łączna liczba podziałów.
+
+    Zwraca:
+        function: Funkcja e'(x) jako lambda.
+    '''
+    x_i = 2 * i / N
+    next_x = x_i + 2 / N
+    previous_x = x_i - 2 / N
+
+    # tbc
+
+
+    
+def E(x):
+    if x<=1: return 2
+    return 6
+
+# def B(i,j, N):
+#    4 * e(i, N, 0) * e(j, N, 0) - gauss_legendre_integral()
